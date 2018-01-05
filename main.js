0076f38eba149a1ca6f8a9b69f965a0cfa6d66f3
crashmandu33 <35149915+crashmandu33@users.noreply.github.com>
2018-01-05 21:06:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ bot.on('ready', () => {
     console.log("Pret");
 });
 
-bot.login('Mzk4NTcyMjA0ODgwNTYwMTI5.DTAfFg.xePyT2ohwbzkFQ_cYqU-hqL4aZM');
+bot.login('');
 bot.on('message', message => {
     if (message.content === ("salope"))
         message.delete()
